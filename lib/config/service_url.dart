a6fb4007ec414253491d4cc06ecb3981c9750025
FOnlyJack <987582940@qq.com>
2019-07-02 16:21:58 +0800

    热搜搜索页面修改

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -3,5 +3,6 @@ const serviceUrl= 'https://www.wanandroid.com/';
 const servicePath={
   'homePageBanner': serviceUrl+'banner/json', // 首页轮播图信息
   'homePageSystem':serviceUrl+'tree/json',//首页体系信息
+  'hotsearch':serviceUrl+'hotkey/json',//热搜内容
 };
 
--- lib/pages/search_screen.dart
+++ lib/pages/search_screen.dart
@@ -5,9 +5,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/config/GlobalConfig.dart';
 import 'package:flutter_demo/mode/HotSearchBean.dart';
+import 'package:flutter_demo/net/service_method.dart';
 import 'package:flutter_demo/pages/search_detail_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -43,7 +45,7 @@ class _SearchPageState extends State<SearchPage> {
               tag: "search",
               child: Container(
                   alignment: Alignment.center,
-                  height: 50,
+                  height: ScreenUtil().setHeight(140),
                   child: Material(
                     color: Colors.transparent,
                     child: TextField(
@@ -53,13 +55,14 @@ class _SearchPageState extends State<SearchPage> {
                       textAlign: TextAlign.left,
                       cursorColor: GlobalConfig.fontColor,
                       style: TextStyle(
-                          color: GlobalConfig.fontColor, fontSize: 16),
+                          color: GlobalConfig.fontColor,
+                          fontSize: ScreenUtil().setSp(42)),
                       maxLines: 1,
                       keyboardType: TextInputType.text,
                       decoration: InputDecoration(
                           suffix: Container(
-                            height: 25,
-                            width: 40,
+                            height: ScreenUtil().setHeight(60),
+                            width: ScreenUtil().setWidth(120),
                             alignment: Alignment.center,
                             child: FlatButton(
                               onPressed: () => {
@@ -68,7 +71,8 @@ class _SearchPageState extends State<SearchPage> {
                                   },
                               child: Text(
                                 "x",
-                                style: TextStyle(fontSize: 12),
+                                style:
+                                    TextStyle(fontSize: ScreenUtil().setSp(24)),
                               ),
                               color: Colors.transparent,
                               textColor: GlobalConfig.fontColor,
@@ -83,7 +87,8 @@ class _SearchPageState extends State<SearchPage> {
                           contentPadding: EdgeInsets.only(left: 20),
                           hintText: "搜索关键词以空格隔开",
                           hintStyle: TextStyle(
-                              color: GlobalConfig.fontColor, fontSize: 16),
+                              color: GlobalConfig.fontColor,
+                              fontSize: ScreenUtil().setSp(42)),
                           border: InputBorder.none),
                     ),
                   ),
@@ -118,156 +123,184 @@ class _SearchPageState extends State<SearchPage> {
           child: Container(
             color: GlobalConfig.cardBackgroundColor,
             padding: EdgeInsets.all(10),
-            width: double.infinity,
-            height: double.infinity,
+            width: ScreenUtil().width,
+            height: ScreenUtil().height,
             child: Column(
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "热搜",
-                      style: TextStyle(
-                          fontSize: 20, color: GlobalConfig.dark?GlobalConfig.fontColor:Theme.of(context).primaryColor),
-                    ),
-                  ],
-                ),
-                Wrap(
-                  spacing: 4,
-                  runSpacing: 1,
-                  alignment: WrapAlignment.start,
-                  children: List.generate(_hotList.length, (i) {
-                    var random = Random(i);
-                    var _color = Color.fromRGBO(random.nextInt(255),
-                        random.nextInt(255), random.nextInt(255), GlobalConfig.dark?0.2:1);
-                    return FlatButton(
-                      onPressed: () =>{
-                        startPageForResult(_hotList[i].name)
-                      },
-                      child: Text(_hotList[i].name),
-                      color: _color,
-                      textColor: GlobalConfig.fontColor,
-                      shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: _color,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(5)),
-                    );
-                  }),
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: Row(
-                      children: <Widget>[
-                        Text(
-                          "搜索历史",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: GlobalConfig.dark?GlobalConfig.fontColor:Theme.of(context).primaryColor),
-                        )
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.start,
-                    )),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          clearHistory();
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Icon(
-                              Icons.delete_forever,
-                              size: 30,
-                              color: GlobalConfig.fontColor,
-                            ),
-                            Text("清空",style: TextStyle(
-                              color: GlobalConfig.fontColor
-                            ),)
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Expanded(
-                    child: Container(
-                  margin: EdgeInsets.only(top: 5),
-                  child: Stack(
-                    children: <Widget>[
-                      Offstage(
-                        offstage: _historyList.length != 0,
-                        child: Container(
-                          alignment: Alignment.topCenter,
-                          child: Text("暂无搜索数据!",style: TextStyle(
-                            color: GlobalConfig.fontColor
-                          ),),
-                        ),
-                      ),
-                      Offstage(
-                        offstage: _historyList.length == 0,
-                        child: ListView.builder(
-                            itemCount: _historyList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return GestureDetector(
-                                onTap: () =>
-                                    {startPageForResult(_historyList[index])},
-                                child: Container(
-                                    height: 50,
-                                    color: Colors.transparent,
-                                    alignment: Alignment.center,
-                                    child: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                            flex: 1,
-                                            child: Row(
-                                              children: <Widget>[
-                                                Icon(
-                                                  Icons.history,
-                                                  size: 30,
-                                                  color: GlobalConfig.fontColor,
-                                                ),
-                                              ],
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                            )),
-                                        Expanded(
-                                          flex: 1,
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.end,
-                                            children: <Widget>[
-                                              Text(
-                                                _historyList[index],
-                                                style: TextStyle(fontSize: 14,color: GlobalConfig.fontColor),
-                                              )
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    )),
-                              );
-                            }),
-                      )
-                    ],
-                  ),
-                ))
+                _hotsearch(),
+                _wraplist(),
+                _searchhistory(),
+                _searchhistorycontent(),
               ],
             ),
           ),
         ));
   }
 
+  ///热搜
+  Widget _hotsearch() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "热搜",
+          style: TextStyle(
+              fontSize: ScreenUtil().setSp(48),
+              color: GlobalConfig.dark
+                  ? GlobalConfig.fontColor
+                  : Theme.of(context).primaryColor),
+        ),
+      ],
+    );
+  }
+
+  ///热搜子项
+  Widget _wraplist() {
+    if (_hotList.length != 0) {
+      List<Widget> listWidget = List.generate(_hotList.length, (i) {
+        var random = Random(i);
+        var _color = Color.fromRGBO(random.nextInt(255), random.nextInt(255),
+            random.nextInt(255), GlobalConfig.dark ? 0.2 : 1);
+        return FlatButton(
+          onPressed: () => {startPageForResult(_hotList[i].name)},
+          child: Text(_hotList[i].name),
+          color: _color,
+          textColor: GlobalConfig.fontColor,
+          shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: _color,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(5)),
+        );
+      });
+      return Wrap(
+        spacing: 4,
+        runSpacing: 1,
+        alignment: WrapAlignment.start,
+        children: listWidget,
+      );
+    } else {
+      return Text("");
+    }
+  }
+
+  ///搜索历史标题
+  Widget _searchhistory() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Row(
+          children: <Widget>[
+            Text(
+              "搜索历史",
+              style: TextStyle(
+                  fontSize: ScreenUtil().setSp(48),
+                  color: GlobalConfig.dark
+                      ? GlobalConfig.fontColor
+                      : Theme.of(context).primaryColor),
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.start,
+        )),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              clearHistory();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(
+                  Icons.delete_forever,
+                  size: 28,
+                  color: GlobalConfig.fontColor,
+                ),
+                Text(
+                  "清空",
+                  style: TextStyle(color: GlobalConfig.fontColor),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  ///搜索历史列表
+  Widget _searchhistorycontent() {
+    return Expanded(
+        child: Container(
+      margin: EdgeInsets.only(top: 5),
+      child: Stack(
+        children: <Widget>[
+          Offstage(
+            offstage: _historyList.length != 0,
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "暂无搜索数据!",
+                style: TextStyle(color: GlobalConfig.fontColor),
+              ),
+            ),
+          ),
+          Offstage(
+            offstage: _historyList.length == 0,
+            child: ListView.builder(
+                itemCount: _historyList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () => {startPageForResult(_historyList[index])},
+                    child: Container(
+                        height: ScreenUtil().setHeight(100),
+                        color: Colors.transparent,
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                                flex: 1,
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.history,
+                                      size: 30,
+                                      color: GlobalConfig.fontColor,
+                                    ),
+                                  ],
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                )),
+                            Expanded(
+                              flex: 1,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    _historyList[index],
+                                    style: TextStyle(
+                                        fontSize: ScreenUtil().setSp(38),
+                                        color: GlobalConfig.fontColor),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        )),
+                  );
+                }),
+          )
+        ],
+      ),
+    ));
+  }
+
   Future getHotSearch() async {
-    Dio dio = new Dio();
-    Response response =
-        await dio.get("https://www.wanandroid.com//hotkey/json");
-    HotSearchBean hotSearchBean = HotSearchBean.fromJson(response.data);
-    setState(() {
-      _hotList.clear();
-      _hotList.addAll(hotSearchBean.data);
+    request('hotsearch').then((val) {
+      HotSearchBean hotSearchBean = HotSearchBean.fromJson(val);
+      setState(() {
+        _hotList.clear();
+        _hotList.addAll(hotSearchBean.data);
+      });
     });
   }
 
